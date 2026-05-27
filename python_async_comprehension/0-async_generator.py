ee47fd0bb5580d497eb212a32c0f913cab3d35c7
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-05-27 13:31:48 +0200

    Create async generator yielding random values with delay

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module defines an async generator that yields random numbers
+with a 1-second asynchronous delay between each yield.
+"""
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    Asynchronously generates 10 random numbers between 0 and 10.
+
+    Yields:
+        float: random number between 0 and 10
+    """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
