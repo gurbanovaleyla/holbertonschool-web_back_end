8176f2bcea1bc57e30a265479eb98aa3b7bcb88a
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-06-23 19:33:36 +0400

    Handle multiple promises with allSettled for profile signup flow

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,18 +5,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((item) => {
-      if (item.status === "fulfilled") {
-        return {
-          status: item.status,
-          value: item.value,
-        };
-      }
-      return {
-        status: item.status,
-        reason: item.reason,
-      };
-    })
-  );
+  ]);
 }
