8ba955fc9866909ea45fa8c41a6b9f588f63f1c9
Leyla Gurbanova <ms.leila.mahammadova@gmail.com>
2026-06-23 20:45:41 +0400

    Handle user input from stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
+  const name = data.toString().replace(/\r?\n/, '');
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
